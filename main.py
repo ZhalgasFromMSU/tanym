c261cc7258a97e6876feb06a0680d15c9778e164
Zhalgas <zhalgas99@outlook.com>
2020-09-16 14:32:32 +0600

    added doctors registration update

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from collections import defaultdict
 from config import problem_types, TOKEN, db_user, db_password, pamyatka
 
 #TODO clients_dict и doctors_dict будут жрать память, пока не закончится регистрация
-#TODO предупреждать клиента, если врача не нашли
+#TODO предупреждать клиента, если врача не нашли, попросить поменять параметры
+#TODO разбить все get функции на ask и save
 
 mydb = mysql.connector.connect(
     host='localhost',
@@ -20,6 +21,7 @@ cursor = mydb.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS psychologists ("
                "problem_type VARCHAR(100), "
                "client_sex INT, " #1 - муж/жен, 2 - жен, 3 - муж
+               "client_lang INT, " #1 - рус/каз, 2 - рус, 3 - каз
                "chat_id VARCHAR(100), "
                "name VARCHAR(100))")
 
@@ -31,7 +33,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS clients ("
                "type VARCHAR(100), "
                "description VARCHAR(300), "
                "status INT, "
-               "review VARCHAR(500))") # status 0 sent, 1 assigned, 2 helped
+               "review VARCHAR(500))") # status 0 sent, 1 helped
 
 cursor.execute("CREATE TABLE IF NOT EXISTS assignments ("
                "client_id VARCHAR(100), "
@@ -75,6 +77,15 @@ def get_name(message):
 
 def get_sex(message):
     clients_dict[message.chat.id]['sex'] = (3 if message.text == 'Мужской' else 2)
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for lang in ('Русский', 'Казахский'):
+        keyboard.row(lang)
+    bot.send_message(message.chat.id, text='Какой вы предпочитаете язык общения?', reply_markup=keyboard)
+    bot.register_next_step_handler(message, get_lang)
+
+
+def get_lang(message):    
+    clients_dict[message.chat.id]['lang'] = (3 if message.text == 'Казахский' else 2)
     bot.send_message(message.chat.id, 'Какой у вас возраст? (Число цифрами, например 29)')
     bot.register_next_step_handler(message, get_age)
 
@@ -111,21 +122,28 @@ def get_problem_type(message):
 
 def get_problem_description(message):
     clients_dict[message.chat.id]['description'] = message.text
+    register_client(clients_dict[message.chat.id], message.chat.id)
+    if send_arrangement(clients_dict[message.chat.id], message.chat.id):
+        bot.send_message(message.chat.id, 'Я отправил сообщение психологам. '
+                                          'Напишу вам, как кто-нибудь откликнется')
+    else:
+        bot.send_message(message.chat.id, 'К сожалению для вас не нашлось психолога. '
+                                          'Попробуйте поменять тип проблемы или язык общения')
+    del clients_dict[message.chat.id]
+
+
+def register_client(client, chat_id):
     cmd = ("INSERT INTO clients "
           "(chat_id, city, sex, age, type, description, status, review) "
           "VALUES (%s, %s, %s, %s, %s, %s, 0, '')")
-    vals = (message.chat.id,
-            clients_dict[message.chat.id]['city'][:98],
-            clients_dict[message.chat.id]['sex'],
-            clients_dict[message.chat.id]['age'][:28],
-            clients_dict[message.chat.id]['type'][:98],
-            clients_dict[message.chat.id]['description'][:298])
+    vals = (chat_id,
+            client['city'][:98],
+            client['sex'],
+            client['age'][:28],
+            client['type'][:98],
+            client['description'][:298])
     cursor.execute(cmd, vals)
     mydb.commit()
-    bot.send_message(message.chat.id, 'Я отправил сообщение психологам. '
-                                      'Напишу вам, как кто-нибудь откликнется')
-    send_arrangement(clients_dict[message.chat.id], message.chat.id)
-    del clients_dict[message.chat.id]
 
 
 def send_arrangement(client, chat_id):
@@ -136,17 +154,20 @@ def send_arrangement(client, chat_id):
     message_text = "\n".join("{0}: {1}".format(ru_key, client[key]) for key, ru_key in keys)
     cmd = "INSERT INTO assignments (client_id, ps_chat_id, msg_id) VALUES (%s, %s, %s)"
     vals = list()
-    cursor.execute("SELECT chat_id, client_sex FROM psychologists WHERE problem_type='{}'".format(client['type']))
+    cursor.execute("SELECT chat_id, client_sex, client_lang FROM psychologists WHERE problem_type='{}'".format(client['type']))
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text='Беру', callback_data='Yes'))
     keyboard.add(types.InlineKeyboardButton(text='Не беру', callback_data='No'))
-    for chat, sex in cursor:
-        if client['sex'] % sex != 0:
+    for chat, sex, lang in cursor:
+        if client['sex'] % sex != 0 or client['lang'] % lang != 0:
             continue
         msg = bot.send_message(int(chat), message_text, reply_markup=keyboard)
         vals.append((chat_id, msg.chat.id, msg.message_id))
+    if len(vals) == 0:
+        return False
     cursor.executemany(cmd, vals)
     mydb.commit()
+    return True
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data in ('Yes', 'No', 'Helped'))
@@ -219,11 +240,24 @@ def get_password(message):
         bot.send_message(message.chat.id, "Неверный пароль")
         return
     bot.send_message(message.chat.id, "Введите ФИО")
+    bot.register_next_step_handler(message, get_lang)
+
+
+def get_lang(message):
+    doctors_dict[message.chat.id]['name'] = message.text
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for lang in ("Русский", "Казахский", "Русский и казахский"):
+        keyboard.row(lang)
+    bot.send_message(message.chat.id, "На каком языке вы хотите общаться с клиентами?", reply_markup=keyboard)
     bot.register_next_step_handler(message, get_fio)
 
 
 def get_fio(message):
-    doctors_dict[message.chat.id]['name'] = message.text
+    langs = ("Русский и казахский", "Русский", "Казахский")
+    if message.text in langs:
+        doctors_dict[message.chat.id]['client_lang'] = langs.index(message.text) + 1
+    else:
+        doctors_dict[message.chat.id]['client_lang'] = 2
     keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     for row in ("Женщины", "Мужчины", "Мужчины и женщины"):
         keyboard.row(row)
@@ -231,11 +265,13 @@ def get_fio(message):
     bot.register_next_step_handler(message, get_client_sexes)
 
 
-#TODO Если в прошлом она фигню пишет, то здесь упадет прога, надо делать проверку
 def get_client_sexes(message):
     sexes = ("Мужчины и женщины", "Женщины", "Мужчины")
-    doctors_dict[message.chat.id]['client_sex'] = sexes.index(message.text) + 1
-    choose_text = "Введите через пробел области, в которых Вы работаете (например: 3 8 13):\n{0}".format(
+    if message.text in sexes:
+        doctors_dict[message.chat.id]['client_sex'] = sexes.index(message.text) + 1
+    else:
+        doctors_dict[message.chat.id]['client_sex'] = 1
+    choose_text = "Введите через пробел области, в которых Вы работаете (например: 3 8 13), введите 0, если не хотите ничего добавлять:\n{0}".format(
         "\n".join("{0}) {1}".format(i + 1, name) for i, name in enumerate(problem_types)))
     bot.send_message(message.chat.id, choose_text)
     bot.register_next_step_handler(message, get_expertise)
@@ -244,22 +280,34 @@ def get_client_sexes(message):
 def get_expertise(message):
     try:
         doctors_dict[message.chat.id]['expertise'] = [
-            problem_types[int(i) - 1] for i in sorted(message.text.split(), key=int)
+            problem_types[int(i) - 1] for i in sorted(message.text.split(), key=int) if i != '0'
         ]
     except Exception as e:
-        bot.send_message(message.chat.id, "Вы ввели номер неправильно, попробуйте зарегистрироваться еще раз")
+        bot.send_message(message.chat.id, "Вы ввели номера неправильно, попробуйте зарегистрироваться еще раз")
+        del doctors_dict[message.chat.id]
         return
-    cmd = ("INSERT INTO psychologists "
-           "(problem_type, client_sex, chat_id, name) "
-           "VALUES(%s, %s, %s, %s)")
-    vals = [
-        (pr_type, doctors_dict[message.chat.id]['client_sex'], message.chat.id, doctors_dict[message.chat.id]['name'])
-            for pr_type in doctors_dict[message.chat.id]['expertise']
-    ]
-    cursor.executemany(cmd, vals)
-    mydb.commit()
+    register_doctor(doctors_dict[message.chat.id], message.chat.id)
     del doctors_dict[message.chat.id]
     bot.send_message(message.chat.id, "Вы успешно зарегистрированы")
+
+
+def register_doctor(doctor, chat_id):
+    doctor_problems = set()
+    if -1 not in doctor['expertise']:
+        cursor.execute("SELECT problem_type FROM psychologists WHERE chat_id={}".format(chat_id))
+        for pr_type, *_ in cursor:
+            doctor_problems.add(pr_type)
+        cmd = ("INSERT INTO psychologists "
+               "(problem_type, client_sex, client_lang, chat_id, name) "
+               "VALUES(%s, %s, %s, %s, %s)")
+        vals = [
+            (pr_type, doctor['client_sex'], doctor['client_lang'], chat_id, doctor['name'])
+                for pr_type in doctor['expertise'] if pr_type not in doctor_problems
+        ]
+        cursor.executemany(cmd, vals)
+    cursor.execute("UPDATE psychologists SET client_sex={0}, client_lang={1} "
+                   "WHERE chat_id={2}".format(doctor['client_sex'], doctor['client_lang'], chat_id))
+    mydb.commit()
 
 
 bot.polling()
